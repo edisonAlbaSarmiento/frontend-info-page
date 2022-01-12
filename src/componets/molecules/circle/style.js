982f3ee3caaf4842c89ac96e4988d8ba65b84e11
edisonalba <edi9708@hotmail.com>
2022-01-11 20:25:23 -0500

    add new style

--- src/componets/molecules/circle/style.js
+++ src/componets/molecules/circle/style.js
@@ -1,25 +1,25 @@
 import styled from 'styled-components';
 
 export const ContentCircle = styled.div`
-  margin: 50px auto;
-  width: 40%;
-  background: white;
+	margin: 50px auto;
+	width: 45%;
+	background: white;
 `;
 
 export const ContentChildren = styled.div`
-  width: 100%;
-  margin-top: 12%;
-  display: flex;
-  justify-content: center;
+	width: 100%;
+	margin-top: 12%;
+	display: flex;
+	justify-content: center;
 `;
 
 export const ContentTitles = styled.div`
-  width: 80%;
-  height: 80%;
-  border-radius: 50%;
-  overflow: hidden;
-  display: flex;
-  justify-content: flex-end;
-  align-items: center;
-  flex-direction: column;
+	width: 80%;
+	height: 80%;
+	border-radius: 50%;
+	overflow: hidden;
+	display: flex;
+	justify-content: flex-end;
+	align-items: center;
+	flex-direction: column;
 `;
